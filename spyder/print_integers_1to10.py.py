3d39bb51528a3ab41c254c9f619356abd3a061e8
zzzoohny <91113238+zzzoohny@users.noreply.github.com>
2021-11-16 01:39:43 +0800

    Add files via upload

---
+++ spyder/print_integers_1to10.py.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a Spyder project containing a Python script that prints all
+integers between 1 and 10 (including 1 and 10).
+
+"""
+
+for x in range(1,11):
+    print(x)
+    
